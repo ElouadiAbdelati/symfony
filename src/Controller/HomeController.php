3a2a67e737d798b58d8ae481021f47840fbbc797
EL ouadi Abdelati <Elouadi_Abdelati@outlook.fr>
2022-05-14 17:41:00 +0100

    push

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,20 +7,20 @@ use App\Repository\VilleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-    /**
-     * @Route("/accuiel")
-     */
+/**
+ * @Route("/accuiel")
+ */
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
     public function index(VilleRepository $villeRepository)
-    { 
+    {
         $villes = $villeRepository->findAll();
-      
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController','villes'=>$villes,
+            'controller_name' => 'HomeController', 'villes' => $villes,
         ]);
     }
 }
